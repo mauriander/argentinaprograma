c65114a60c1f94cf1c2d37feba3b025222b3ce17
mauriander <andermattenmauricio@gmail.com>
2023-05-30 19:50:46 -0300

    RESTA Y MULTIPLICACION

---
+++ Practico0/multiplicacion.js
@@ -0,0 +1,7 @@
+let a;
+let b;
+console.log("Ingrese el primer numero, Minuendo");
+
+console.log("Ingrese el segundo numero, Sustraendo");
+
+console.log(a*b);
---
+++ Practico0/resta.js
@@ -0,0 +1,10 @@
+let a;
+let b;
+//console.log("Ingrese el primer numero");
+
+//console.log("Ingrese el segundo numero");
+
+a=10;
+b=2;
+
+console.log(a-b);
