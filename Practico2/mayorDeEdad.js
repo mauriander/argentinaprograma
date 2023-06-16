f1e4ff636aedfaf64aa81b8e7afe7eedc7645ec7
mauriander <andermattenmauricio@gmail.com>
2023-06-15 23:15:35 -0300

    ejercicio Mayor de edad

---
+++ Practico2/mayorDeEdad.js
@@ -0,0 +1,23 @@
+const readlineSync = require("readline-sync");
+
+const edad= readlineSync.question("Ingrese la edad ");
+
+if(edad >17){
+    console.log("Eres mayor Edad");
+}else{
+   console.log("Eres menor de Edad");
+}
+
+if(edad>0){
+    console.log("El numero es positivo");
+}  else if(edad<0){
+    console.log("El numero es negativo");
+        }
+        else{console.log("El numero es cero");}
+
+//console.log("La edad ingresada fue " + edad);
+
+
+
+//edad>17 ?  console.log("es mayor") : console.log("es menor");
+
